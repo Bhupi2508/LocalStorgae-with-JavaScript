22896bbee516fe5bf7f47ac42e7d97ec130f9e7e
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2020-10-21 20:19:09 +0530

    value set & get in localStorage

--- app.js
+++ app.js
@@ -11,9 +11,26 @@ listenerValue();
 
 // read the input value from the user during add
 function listenerValue() {
+
+    // Listener for add element
     document.querySelector('#form').addEventListener('submit', DataValue);
 
+    // Listener for remove element
     printData.addEventListener('click', DataRemoveValue);
+
+    // Listener for on-load localStorage data print
+    // jQuery function
+    // let a = window.addEventListener('DOMContentLoaded', localStorageOnLoad)
+    // console.log("a", a);
+
+    if (document.readyState !== 'loading') init()
+else document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    console.log("Do it !");
+
+}
+
 }
 
 /**
@@ -45,19 +62,96 @@ function DataValue(e) {
     // print data on page
     printData.appendChild(ol)
 
+    // fn where we add the value in localStorage
+    addLocalStorage(dataValue);
+
+
 }
 
 /**
- * Desc : // calling this fn from addEventListerners during remome value
+ * Desc : calling this function from addEventListerners during remome value
  * @param {*} e 
  */
 function DataRemoveValue(e) {
 
     // find the excat value
-    if(e.target.classList.contains('remove-data')){
+    if (e.target.classList.contains('remove-data')) {
 
         // remove parent tag for that value
         e.target.parentElement.remove()
     }
+
+}
+
+
+/**
+ * Desc : function where we add the value in localStorage
+ * @param {*} e 
+ */
+function addLocalStorage(dataValue) {
+
+    let datafromLocal = getLocalStorage();
+    datafromLocal.push(dataValue)
+
+
+    // add in localStorage with JSON format
+    localStorage.setItem('names', JSON.stringify(datafromLocal))
+
+}
+
+
+/**
+ * Desc : get the value from lcoalStorage
+ * @param {*}  
+ */
+function getLocalStorage() {
+    let data;
+
+    // get the value from local Storage
+    const localData = localStorage.getItem('names');
+
+    if (localData === null) {
+        data = [];
+    } else {
+        data = JSON.parse(localData)
+    }
+
+    return data;
+}
+
+
+/**
+ * Desc : calling this function from addEventListerners during remome value
+ * @param {*} e 
+ */
+function localStorageOnLoad() {
+
+    // call the getLocalStorage function
+    let onLoadData = getLocalStorage();
+    console.log(onLoadData);
+
+    // loop for print all the data
+    onLoadData.forEach(dataValue => {
+        console.log("dataValue");
+
+        // add a remove button with the value
+        const removeButton = document.createElement('a');
+        removeButton.classList = 'remove-data';
+        removeButton.textContent = 'remove';
+
+        // const = document.createElement('li')
+        const li = document.createElement('li');
+
+        li.textContent = dataValue;
+
+        // append the remove button with the data
+        li.appendChild(removeButton)
+
+        // append li tag to ol tag
+        ol.appendChild(li);
+
+        // print data on page
+        printData.appendChild(ol)
+    });
 
 }
