92d555e78cab1a18204c66e529032b6b0e883d14
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2020-10-21 13:13:00 +0530

    add and remoce function added but not from localstorage

--- app.js
+++ app.js
@@ -3,34 +3,61 @@ const printData = document.querySelector('#data')
 
 // create a li tag for print the data one by one
 const ol = document.createElement('ol')
+// assign ol Id to ol tag
+ol.id = 'ol';
 
 // call the funstion
-listenerValue()
+listenerValue();
 
-// read the input value from the user
+// read the input value from the user during add
 function listenerValue() {
-    document.querySelector('form').addEventListener('submit', DataValue);
+    document.querySelector('#form').addEventListener('submit', DataValue);
+
+    printData.addEventListener('click', DataRemoveValue);
 }
 
-// calling this fn from addEventListerners
+/**
+ * Desc : // calling this fn from addEventListerners during add value
+ * @param {*} e 
+ */
 function DataValue(e) {
     e.preventDefault();
 
     //get the input value
     const dataValue = document.querySelector('#name').value;
 
-    // assign ol Id to ol tag
-    ol.id = 'ol';
+    // add a remove button with the value
+    const removeButton = document.createElement('a');
+    removeButton.classList = 'remove-data';
+    removeButton.textContent = 'remove';
 
     // const = document.createElement('li')
     const li = document.createElement('li');
 
     li.textContent = dataValue;
 
+    // append the remove button with the data
+    li.appendChild(removeButton)
+
     // append li tag to ol tag
-    ol.appendChild(li)
+    ol.appendChild(li);
 
     // print data on page
     printData.appendChild(ol)
+
+}
+
+/**
+ * Desc : // calling this fn from addEventListerners during remome value
+ * @param {*} e 
+ */
+function DataRemoveValue(e) {
+
+    // find the excat value
+    if(e.target.classList.contains('remove-data')){
+
+        // remove parent tag for that value
+        e.target.parentElement.remove()
+    }
 
 }
